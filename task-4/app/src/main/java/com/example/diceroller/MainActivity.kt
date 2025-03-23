747985dfbaf08e3bf9b4112c7f09eb3602f2c74b
clarkkkkk <juanfarrel0404@gmail.com>
2025-03-24 02:58:29 +0700

    feat: add task 4 - make dice roller apps

---
+++ task-4/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+                DiceRollerApp()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize().background(color = Color(0xFFFFF8DE))
+        )
+    }
+    DiceWithButtonAndImage(
+        modifier = Modifier.
+        fillMaxSize().
+        wrapContentSize(Alignment.Center)
+    )
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+    var shuffled by remember { mutableStateOf(false) }
+
+    val animation by animateFloatAsState(
+        targetValue = if (shuffled) 360f else 0f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessMedium
+        )
+    )
+
+    LaunchedEffect(animation) {
+        if (shuffled) {
+            shuffled = false
+        }
+    }
+    
+    val imageRes = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = "dice_$result",
+            modifier = Modifier.rotate(animation)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = "Anda mendapatkan angka: $result",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            result = (1..6).random()
+            shuffled = true
+        }, modifier = Modifier.size(120.dp, 50.dp) ) {
+            Text(text = stringResource(R.string.roll), fontSize = 18.sp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewDiceRollerApp() {
+    DiceRollerTheme {
+        DiceRollerApp()
+    }
+}
+
